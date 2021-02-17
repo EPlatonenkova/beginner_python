6285583485e73295a24568c0a8363a9897379202
EPlatonenkova <esurkova99@mail.ru>
2021-02-17 21:04:55 +0300

    6 дз новая папка

---
+++ lesson_6/6-1.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+
+class TrafficLight:
+    _color = ["Красный", "Желтый", "Зеленый"]
+
+    def running(self):
+        count = 0
+        while True:
+            print(f"{self._color[count]}")
+            if count == 0:
+                sleep(7)
+            elif count == 1:
+                sleep(2)
+            elif count == 2:
+                sleep(10)
+                count = 0
+                continue
+            count += 1
+
+
+# TrafficLight.running()
+tl = TrafficLight()
+tl.running()
---
+++ lesson_6/6-2.py
@@ -0,0 +1,11 @@
+class Road:
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+
+    def __init__(self, mass):
+        self._mass = mass
+
+
+
+
